33c3687efd4fe7c316b94bc112215d6d695f4541
Ali Khosronejad <khosronejadali344@gmail.com>
2024-05-25 13:39:59 +0330

    banner section added to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/navbar/Navbar"
+import Banner from "./components/banner/Banner"
 
 
 
@@ -6,6 +7,7 @@ function App() {
     return (
       <>
         <Navbar/>
+        <Banner/>
       </>
   )
 }
--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,7 +1,13 @@
+import BannerItem from "./BannerItem"
+
 export default function Banner(){
    return(
-        <div className="pt-2.5 px-4 pb-6 flex flex-wrap " >
-            
+        <div className="pt-2.5 px-4 pb-6 flex flex-wrap  lg:[&>:nth-child(1)]:w-[58%] lg:[&>:nth-child(2)]:w-[40%] lg:[&>:nth-child(3)]:w-[49%] lg:[&>:nth-child(4)]:w-[49%]" >
+            <BannerItem img="/img/fesharkhoon.webp" />
+            <BannerItem img="/img/soup.webp" />            
+            <BannerItem img="/img/laghari.webp" />            
+            <BannerItem img="/img/ofonat.webp" />            
+
         </div>
    ) 
 }
