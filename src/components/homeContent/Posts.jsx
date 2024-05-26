aef3cd8cdf30127e84630ef29f1a8ba6d8d6cc3b
Ali Khosronejad <khosronejadali344@gmail.com>
2024-05-26 17:23:59 +0330

    post.jsx created

---
+++ src/components/homeContent/Posts.jsx
@@ -0,0 +1,7 @@
+export default function Posts(){
+    return(
+        <div className="pt-2.5  pb-6 flex flex-wrap  lg:[&>:nth-child(1)]:w-[58%] lg:[&>:nth-child(2)]:w-[40%] lg:[&>:nth-child(3)]:w-[49%] lg:[&>:nth-child(4)]:w-[49%]">
+            
+        </div>
+    )
+}
