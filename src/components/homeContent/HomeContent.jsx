66c264d41812c0d0f86e94456119c4d07de08ce3
Ali Khosronejad <khosronejadali344@gmail.com>
2024-05-26 17:15:30 +0330

    homecontent.jsx added to homepage

---
+++ src/components/homeContent/HomeContent.jsx
@@ -0,0 +1,12 @@
+export default function HomeContent(){
+    return(
+        <div className="flex">
+                <div>
+                    <h1>جدید ترین مطالب</h1>
+                </div>
+                <aside>
+
+                </aside>
+        </div>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import Banner from "../components/banner/Banner"
+import HomeContent from "../components/homeContent/HomeContent"
 export default function HomePage(){
     return(
         <>
             <Banner/>
+            <HomeContent/>
         </>
     )
 }
