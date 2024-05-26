5559420bb992c36e611fb5112c1e9416c02e289c
Ali Khosronejad <khosronejadali344@gmail.com>
2024-05-26 20:24:50 +0330

    reponsive homecontent.jsx

--- src/components/homeContent/HomeContent.jsx
+++ src/components/homeContent/HomeContent.jsx
@@ -1,8 +1,8 @@
 import Posts from "./Posts"
-
+import List from "./aside/List"
 export default function HomeContent(){
     return(
-        <div className="flex justify-between">
+        <div className=" flex flex-col px-6 lg:px-[0px] lg:flex-row justify-between">
                 <div className="w-[100%] lg:w-[70%] ">
                     <h1 className="py-4 px-2 text-green-500 text-xl">جدید ترین مطالب</h1>  
                     <Posts/>
@@ -13,13 +13,14 @@ export default function HomeContent(){
                     <Posts/>        
                     <Posts/>        
                     <Posts/>        
-
-
-
                 </div>
 
 
-                <aside className="w-[100%] lg:w-[30%] bg-yellow-900">
+                <aside className="w-[100%] lg:w-[30%] [&>:nth-child(4)]:sticky [&>:nth-child(4)]:top-4">
+                    <List/>
+                    <List/>
+                    <List/>
+                    <List/>
 
                 </aside>
         </div>
