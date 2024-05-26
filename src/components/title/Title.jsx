7b093d367d554719774cae52d42803c29ebe8553
Ali Khosronejad <khosronejadali344@gmail.com>
2024-05-26 17:21:00 +0330

    title.jsx created

---
+++ src/components/title/Title.jsx
@@ -0,0 +1,16 @@
+export default function Title(){
+    return(
+        <div>
+            <div>
+                <h1 className="bg-orange-500 text-white text-base">تناسب اندام</h1>
+            </div>
+
+            <div className="flex items-center">
+                <div className="w-[60%]"></div>
+                <div className="w-[20%]"></div>
+                <div className="w-[15%]"></div>
+                <div className="w-[5%]"></div>
+            </div>
+        </div>
+    )
+}
