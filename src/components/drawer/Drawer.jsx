77551056ab1fd64539a49a270481b3413c4651b6
Ali Khosronejad <khosronejadali344@gmail.com>
2024-05-27 21:41:43 +0330

    drawermenu some edites

--- src/components/drawer/Drawer.jsx
+++ src/components/drawer/Drawer.jsx
@@ -4,7 +4,7 @@ import Item from "./Item"
 export default function Drawer({drawer}){
     return(
 
-        <div className={` ${drawer ?"overflow-visible max-h-max ": "overflow-hidden max-h-[0px] " } bg-white  w-[92%] left-[4%] right-[%4] px-4 py-2 shadow-xl rounded-lg  fixed z-50 top-[70px] bakh-reg lg:hidden my-transition`}>
+        <div className={` ${drawer ?"overflow-visible max-h-max ": "overflow-hidden max-h-[0px] opacity-0" } bg-white  w-[92%] left-[4%] right-[%4] px-4 py-2 shadow-xl rounded-lg  fixed z-50 top-[70px] bakh-reg lg:hidden transition-[max-height,opacity] [transition:max-height_200ms,opacity_150ms]`}>
                 <ul>
                     {
                         menuItems.map((item , index )=><Item key={index} data={item}/>)
@@ -14,3 +14,6 @@ export default function Drawer({drawer}){
       
     )
 }
+
+//  shadow-[0px_1px_1px_1px_rgba(0,0,0,.16)]
+//  transition-[transform,visibility] [transition:transform_300ms,visibility_150ms]  linear 
