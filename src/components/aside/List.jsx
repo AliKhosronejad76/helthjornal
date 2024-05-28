2233fff0a65dd627697b0d471b8283ec8a8428b2
Ali Khosronejad <khosronejadali344@gmail.com>
2024-05-28 11:48:08 +0330

    asidefolder  directory changed

---
+++ src/components/aside/List.jsx
@@ -0,0 +1,19 @@
+import Title from "../title/Title"
+import Item from "./Item"
+
+export default function List(){
+    return(
+        <div>
+            <Title/>
+
+            <ul className="">
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+                <Item/>
+
+            </ul>
+        </div>
+    )
+}
